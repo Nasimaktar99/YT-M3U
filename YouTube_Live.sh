d51ac6ad8eee687bc94248728edf3778c32321ea
Nasim Aktar <67604361+Nasimaktar99@users.noreply.github.com>
2024-06-24 17:28:15 +0530

    Update YouTube_Live.sh

--- YouTube_Live.sh
+++ YouTube_Live.sh
@@ -1,14 +1,10 @@
-#!/bin/bash 
- 
-# Navigate to the directory of the script 
-SCRIPT_DIR=$(dirname $0) 
-echo $SCRIPT_DIR 
- 
-# Install the required Python package 
-python3 -m pip install requests --quiet 
- 
-# Change to the scripts directory 
-cd $SCRIPT_DIR/scripts/ 
+#!/bin/bash
+
+echo $(dirname $0)
+
+python3 -m pip install requests
+
+cd $(dirname $0)/scripts/
  
 # Run the modified Python script and output to a single m3u file 
 python3 YouTube_Live.py > ../YouTube_Live.m3u 
