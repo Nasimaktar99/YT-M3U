36bd4e5ca5c65c79db633a6fd0d1f938d6d87d4d
Nasim Aktar <67604361+Nasimaktar99@users.noreply.github.com>
2024-06-26 21:22:55 +0530

    Update YouTube_Live.sh

--- YouTube_Live.sh
+++ YouTube_Live.sh
@@ -5,8 +5,7 @@ echo $(dirname $0)
 python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
- 
-# Run the modified Python script and output to a single m3u file 
-python3 YouTube_Live.py > ../YouTube_Live.m3u 
- 
-echo "m3u file generated"
+
+python3 IFAS_Live.py > ../IFAS_Live.m3u8
+
+echo m3u8 grabbed
